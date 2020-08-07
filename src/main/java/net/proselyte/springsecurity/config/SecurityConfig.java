59c9728769fbb8ee3c409bdcbe1d57a9e915c99b
Eugene Suleimanov <eugene.s@openlegacy.com>
2020-08-07 14:18:22 +0300

    STEP2: Added index page and configured access

--- src/main/java/net/proselyte/springsecurity/config/SecurityConfig.java
+++ src/main/java/net/proselyte/springsecurity/config/SecurityConfig.java
@@ -12,6 +12,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
