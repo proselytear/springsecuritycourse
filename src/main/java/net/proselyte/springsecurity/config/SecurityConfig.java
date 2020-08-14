3005dbee89244c37addd6f1cdba8b1c9e1582a29
Eugene Suleimanov <eugene.s@openlegacy.com>
2020-08-14 11:16:32 +0300

    STEP7: Configured form based authentication

--- src/main/java/net/proselyte/springsecurity/config/SecurityConfig.java
+++ src/main/java/net/proselyte/springsecurity/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin();
     }
 
     @Bean
